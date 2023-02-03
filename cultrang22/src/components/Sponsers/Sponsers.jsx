05eaa1d9964b0fce4b2dec32224814cd199dc467
akshad-gajbhiye <over1over2over@gmail.com>
2023-02-03 19:41:37 +0530

    "Added sponsors page(raw)"

--- cultrang22/src/components/Sponsers/Sponsers.jsx
+++ cultrang22/src/components/Sponsers/Sponsers.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import "./sponsers.css"
+import "./sponsers.css";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
 
-export default function Sponsers () {
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
+
+export default function Sponsers() {
     return (
+      <>
+        <h1 style={{ textAlign: "center" }}>Sponsers</h1>
         <div className="Sponsers">
+          <Carousel breakPoints={breakPoints}>
+            <Item>Cultrang_Sponser_1</Item>
+            <Item>Cultrang_Sponser_2</Item>
+            <Item>Cultrang_Sponser_3</Item>
+            <Item>Cultrang_Sponser_4</Item>
+            <Item>Cultrang_Sponser_5</Item>
+          </Carousel>
         </div>
+      </>
     );
-}
+  }
