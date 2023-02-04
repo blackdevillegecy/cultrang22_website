eb28cde13a3e9a6b8be64a8b604cb82c4c2659c5
Adwita <adwita5b@gmail.com>
2023-02-04 15:10:09 +0530

    changes made to Events.jsx and events.css

---
+++ cultrang22/src/components/Events/Carousal2.jsx
@@ -0,0 +1,22 @@
+import Carousel from "react-elastic-carousel";
+import React from 'react'
+import "./events.css"
+
+export default function Carousal2() {
+  return (
+      <Carousel>
+            <div className="Events-rect">
+                <img src="assets/1.jpeg"  /> 
+                <p className="legend">Event 1</p>
+            </div>
+            <div className="Events-rect">
+                <img src="assets/2.jpeg" />  
+                <p className="legend">Event 2</p>
+            </div>
+            <div className="Events-rect">
+                <img src="assets/3.jpeg" />
+                <p className="legend">Event 3</p>
+            </div>
+        </Carousel>
+  )
+}
